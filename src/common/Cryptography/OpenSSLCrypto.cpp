d5312e6d189b7af264cedc6131b6e453543ae0b1
Ovahlord <dreadkiller@gmx.de>
2024-05-12 15:48:39 +0200

    Core/Cryptography: dropped support for OpenSSL 1.1
    OpenSSL 1.1 has been deprecated for quite some time now so it is time to put it to rest. Please upgrade to OpenSSL 3.x

--- src/common/Cryptography/OpenSSLCrypto.cpp
+++ src/common/Cryptography/OpenSSLCrypto.cpp
@@ -17,29 +17,23 @@
 
 #include "OpenSSLCrypto.h"
 #include <openssl/crypto.h>
-
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L
 #include <openssl/provider.h>
+
 OSSL_PROVIDER* LegacyProvider;
 OSSL_PROVIDER* DefaultProvider;
-#endif
 
 void OpenSSLCrypto::threadsSetup([[maybe_unused]] boost::filesystem::path const& providerModulePath)
 {
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L
 #if TRINITY_PLATFORM == TRINITY_PLATFORM_WINDOWS
     OSSL_PROVIDER_set_default_search_path(nullptr, providerModulePath.string().c_str());
 #endif
     LegacyProvider = OSSL_PROVIDER_load(nullptr, "legacy");
     DefaultProvider = OSSL_PROVIDER_load(nullptr, "default");
-#endif
 }
 
 void OpenSSLCrypto::threadsCleanup()
 {
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     OSSL_PROVIDER_unload(LegacyProvider);
     OSSL_PROVIDER_unload(DefaultProvider);
     OSSL_PROVIDER_set_default_search_path(nullptr, nullptr);
-#endif
 }
