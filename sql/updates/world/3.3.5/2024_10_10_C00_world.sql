3d1ab3d1aefaaf7e7f4d0ae47ae59928b82fa39c
Grimdhex <176165533+Grimdhex@users.noreply.github.com>
2024-10-10 23:43:58 +0200

    Scripts/MoltenCore: Magmadar improvements

---
+++ sql/updates/world/3.3.5/2024_10_10_C00_world.sql
@@ -0,0 +1,4 @@
+-- Spell script for Lava bomb visual (Magmadar)
+DELETE FROM `spell_script_names` WHERE `spell_id` = 19411;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(19411, 'spell_magmadar_lava_bomb');
--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_magmadar.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_magmadar.cpp
@@ -15,12 +15,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: Boss_Magmadar
-SD%Complete: 75
-SDComment: Conflag on ground nyi
-SDCategory: Molten Core
-EndScriptData */
+/// @note: Compare to Mangos, it's confirmed that Lava bomb spell is the same for melee and ranged targets.
 
 #include "ScriptMgr.h"
 #include "molten_core.h"
@@ -29,85 +24,108 @@ EndScriptData */
 
 enum Texts
 {
-    EMOTE_FRENZY        = 0
+    EMOTE_FRENZY            = 0
 };
 
 enum Spells
 {
-    SPELL_FRENZY        = 19451,
-    SPELL_MAGMA_SPIT    = 19449,
-    SPELL_PANIC         = 19408,
-    SPELL_LAVA_BOMB     = 19428,
+    SPELL_FRENZY                    = 19451,
+    SPELL_MAGMA_SPIT                = 19449,
+    SPELL_PANIC                     = 19408,
+    SPELL_LAVA_BOMB                 = 19411,
+    SPELL_LAVA_BOMB_VISUAL          = 20494
+
 };
 
 enum Events
 {
-    EVENT_FRENZY        = 1,
-    EVENT_PANIC         = 2,
-    EVENT_LAVA_BOMB     = 3,
+    EVENT_FRENZY            = 1,
+    EVENT_PANIC             = 2,
+    EVENT_LAVA_BOMB         = 3
 };
 
 struct boss_magmadar : public BossAI
 {
-    boss_magmadar(Creature* creature) : BossAI(creature, BOSS_MAGMADAR)
-    {
-    }
+    boss_magmadar(Creature* creature) : BossAI(creature, BOSS_MAGMADAR) { }
 
     void Reset() override
     {
         BossAI::Reset();
-        DoCast(me, SPELL_MAGMA_SPIT, true);
+        DoCastSelf(SPELL_MAGMA_SPIT, true);
     }
 
     void JustEngagedWith(Unit* victim) override
     {
         BossAI::JustEngagedWith(victim);
-        events.ScheduleEvent(EVENT_FRENZY, 30s);
-        events.ScheduleEvent(EVENT_PANIC, 20s);
+
+        events.ScheduleEvent(EVENT_FRENZY, 8500ms);
+        events.ScheduleEvent(EVENT_PANIC, 5s, 10s);
         events.ScheduleEvent(EVENT_LAVA_BOMB, 12s);
     }
 
-    void UpdateAI(uint32 diff) override
+    void ExecuteEvent(uint32 eventId) override
     {
-        if (!UpdateVictim())
-            return;
-
-        events.Update(diff);
-
-        if (me->HasUnitState(UNIT_STATE_CASTING))
-            return;
-
-        while (uint32 eventId = events.ExecuteEvent())
+        switch (eventId)
         {
-            switch (eventId)
-            {
-                case EVENT_FRENZY:
-                    Talk(EMOTE_FRENZY);
-                    DoCast(me, SPELL_FRENZY);
-                    events.ScheduleEvent(EVENT_FRENZY, 15s);
-                    break;
-                case EVENT_PANIC:
-                    DoCastVictim(SPELL_PANIC);
-                    events.ScheduleEvent(EVENT_PANIC, 35s);
-                    break;
-                case EVENT_LAVA_BOMB:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 0.0f, true, true, -SPELL_LAVA_BOMB))
-                        DoCast(target, SPELL_LAVA_BOMB);
-                    events.ScheduleEvent(EVENT_LAVA_BOMB, 12s);
-                    break;
-                default:
-                    break;
-            }
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
+            case EVENT_FRENZY:
+                Talk(EMOTE_FRENZY);
+                DoCastSelf(SPELL_FRENZY);
+                events.Repeat(15s);
+                break;
+            case EVENT_PANIC:
+                DoCastVictim(SPELL_PANIC);
+                events.Repeat(30s, 35s);
+                break;
+            case EVENT_LAVA_BOMB:
+                if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 0.0f, true, true, -SPELL_LAVA_BOMB))
+                    DoCast(target, SPELL_LAVA_BOMB);
+                events.Repeat(12s, 15s);
+                break;
+            default:
+                break;
         }
+    }
+};
 
-        DoMeleeAttackIfReady();
+// Lava Bomb - Dummy visual
+class spell_magmadar_lava_bomb : public SpellScript
+{
+    PrepareSpellScript(spell_magmadar_lava_bomb);
+
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo({ SPELL_LAVA_BOMB_VISUAL });
+    }
+
+    void HandleDummy(SpellEffIndex /*effIndex*/)
+    {
+        if (Unit* target = GetHitUnit())
+        {
+            uint32 spellId = 0;
+            switch (m_scriptSpellId)
+            {
+                case SPELL_LAVA_BOMB:
+                {
+                    spellId = SPELL_LAVA_BOMB_VISUAL;
+                    break;
+                }
+                default:
+                {
+                    return;
+                }
+            }
+            target->CastSpell(target, spellId, true);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_magmadar_lava_bomb::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
     }
 };
 
 void AddSC_boss_magmadar()
 {
     RegisterMoltenCoreCreatureAI(boss_magmadar);
+    RegisterSpellScript(spell_magmadar_lava_bomb);
 }
